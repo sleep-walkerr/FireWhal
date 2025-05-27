02b5d55003ca4cfb48d8f916971cede8947d84c6
Paxton <pax@isphere.net>
2025-05-27 14:47:13 -0500

    More experimentation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use color_eyre::{Result};
+use color_eyre::Result;
 use crossterm::event::{KeyCode, KeyEventKind};
 use ratatui::{
-    layout::{Constraint, Layout}, 
-    symbols::border, 
-    text::{Line, Span}, 
-    widgets::{Block, Gauge, Widget}, 
-    style::{Color,Style,Stylize},
-    DefaultTerminal, Frame
+    DefaultTerminal, Frame,
+    layout::{Constraint, Layout},
+    style::{Color, Style, Stylize},
+    symbols::border,
+    text::{Line, Span},
+    widgets::{Block, Gauge, Widget},
 };
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let mut terminal = ratatui::init();
-    let mut app: App = App { 
+    let mut app: App = App {
         exit: false,
-        progress_bar_color: Color::Green
+        progress_bar_color: Color::Green,
     };
     let app_result = app.run(&mut terminal);
     ratatui::restore();
@@ -23,15 +23,14 @@ fn main() -> Result<()> {
 
 pub struct App {
     exit: bool,
-    progress_bar_color: Color
+    progress_bar_color: Color,
 }
-
 
 impl App {
     fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
         while !self.exit {
             match crossterm::event::read()? {
-                crossterm::event::Event::Key(key_event  ) => self.handle_key_event(key_event)?,
+                crossterm::event::Event::Key(key_event) => self.handle_key_event(key_event)?,
                 _ => {}
             }
             terminal.draw(|frame| self.draw(frame))?;
@@ -39,7 +38,7 @@ impl App {
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame){
+    fn draw(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
     }
 
@@ -54,41 +53,43 @@ impl App {
 impl Widget for &App {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
-        Self: Sized {
-            let vertical_layout: Layout = Layout::vertical([Constraint::Percentage(20), Constraint::Percentage(80)]);
-            let [title_area, gauge_area] = vertical_layout.areas(area);
+        Self: Sized,
+    {
+        let vertical_layout: Layout =
+            Layout::vertical([Constraint::Percentage(20), Constraint::Percentage(80)]);
+        let [title_area, gauge_area] = vertical_layout.areas(area);
 
-
-            Line::from("Process overview")
+        Line::from("Process overview")
             .bold()
             .render(title_area, buf);
 
-            let instructions = Line::from(vec![
-                Span::from(" Change color "),
-                " <C> ".blue().bold(),
-                Span::from(" Quit "),
-                " <Q> ".blue().bold()
-            ])
-            .centered();
+        let instructions = Line::from(vec![
+            Span::from(" Change color "),
+            " <C> ".blue().bold(),
+            Span::from(" Quit "),
+            " <Q> ".blue().bold(),
+        ])
+        .centered();
 
-            let block = Block::bordered()
+        let block = Block::bordered()
             .title(Line::from("Background Processes"))
             .title_bottom(instructions)
             .border_set(border::THICK);
 
-            let progress_bar = Gauge::default()
+        let progress_bar = Gauge::default()
             .gauge_style(Style::default().fg(self.progress_bar_color))
             .block(block)
             .label(format!("Process 1: 50%"))
             .ratio(0.5);
-            
 
-            progress_bar.render(ratatui::layout::Rect { x: (gauge_area.left()),
-                 y: (gauge_area.top()), 
-                 width: (gauge_area.width), 
-                 height: (3) }, 
-                 buf
-                );
-
-        }
+        progress_bar.render(
+            ratatui::layout::Rect {
+                x: (gauge_area.left()),
+                y: (gauge_area.top()),
+                width: (gauge_area.width),
+                height: (3),
+            },
+            buf,
+        );
+    }
 }
