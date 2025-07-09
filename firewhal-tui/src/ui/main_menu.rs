de45717bc7f26155f60a66b55507edb96b210115
Paxton <pax@isphere.net>
2025-07-09 12:56:43 -0500

    Deleted original file layout, next commit will be updated

--- firewhal-tui/src/ui/main_menu.rs
+++
@@ -1,8 +0,0 @@
-use ratatui::{prelude::*, widgets::*};
-
-pub fn render(f: &mut Frame) {
-    let block = Block::default()
-        .title("Main Screen")
-        .borders(Borders::ALL);
-    f.render_widget(block, f.area());
-}
